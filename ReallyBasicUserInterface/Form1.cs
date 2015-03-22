57b50df2d8b5963d830086b85aedf4b324ad9ddb
tima-t <tito.marchev@gmail.com>
2015-03-23 01:14:17 +0200

    Basic try for user interface -Windows Forms.

---
+++ ReallyBasicUserInterface/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class CardGame : Form
+    {
+        int a;
+        int b;
+        public CardGame()
+        {
+            InitializeComponent();
+        }
+
+        private void CardGame_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            a = 1;
+        }
+
+
+        private void pictureBox6_MouseEnter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox11_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (a == 1)
+            {
+                pictureBox11.Image = pictureBox6.Image;
+                a = 0;
+                b = 1;
+            }
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_MouseLeave(object sender, EventArgs e)
+        {
+            if (a==1)
+            {
+                pictureBox11.Image = pictureBox6.Image;
+                pictureBox6.Image = null;
+                
+            }
+        }
+    }
+}
