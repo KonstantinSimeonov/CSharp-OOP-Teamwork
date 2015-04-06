1d707b33ec71a3278d81972830be223d5ac187a8
vlad-sp <vladimirsopotlyanov@gmail.com>
2015-04-07 00:12:50 +0300

    Added BinarySerialization, use Serializable attribute for the classes you want to serialize.

---
+++ BinarySerialization/BinarySerialization/BinarySerialization.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace BinarySerialization
+{
+  public class BinarySerialization
+    {
+
+        public string fileContent;
+
+
+        public void Serialize(string pathFrom, string pathTo)
+        {
+            fileContent = File.ReadAllText(pathFrom);
+            var stream = new FileStream(pathTo, FileMode.OpenOrCreate);
+            var binaryFormatter = new BinaryFormatter();
+
+            binaryFormatter.Serialize(stream, fileContent);
+            stream.Close();
+        }
+
+        public string Deserialize(string path)
+        {
+            var stream = new FileStream(path, FileMode.Open);
+            var binaryFormatter = new BinaryFormatter();
+
+            string result = (string)binaryFormatter.Deserialize(stream);
+            stream.Close();
+            return result;
+
+        }
+
+    }
+}
